c7267ff8f73a5bf1020345cbd1209a8aa27f87ea
unknown <ibmjavanh09@iiht.tech>
2021-03-10 12:49:43 +0530

    String and date class

---
+++ StringClass/src/DateClass.java
@@ -0,0 +1,11 @@
+import java.util.Date;
+
+public class DateClass {
+	public static void main(String[] args) {
+		Date date1=new Date(2021,3,10);
+		System.out.println(date1.getDate());
+		System.out.println(date1.getMonth());
+		System.out.println(date1.getYear());
+	}
+
+}
